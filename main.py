98b40acf162da4e142262247140d0f07057472e5
ahmojo <A.ilhan.inf24@stud.bbbaden.ch>
2026-03-12 12:34:36 +0100

    upto Chapter 3 lesson 2

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.__version__}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    my_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                 return  
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = my_clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
